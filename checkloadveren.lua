1b8e4092ac64a88b6417c1aa5f9d15fa98775e60
Biscuit024 <mr.biscuit024@gmail.com>
2019-04-18 15:46:45 +0800

    Delete checkloadveren.lua

--- checkloadveren.lua
+++
@@ -1,6 +0,0 @@
-if loaderver == 3.1 then 
-	load(gg.makeRequest("https://raw.githubusercontent.com/Biscuit024/atggsdfaetasdfgtgafvsefsgawafsgsef/master/TOS_Auto_v3.1.lua").content)()
-end
-if loaderver < 3.1 then 
-	print('※This loader is outdated, please update to TOS_Auto_Loader_v3.0※')
-end
