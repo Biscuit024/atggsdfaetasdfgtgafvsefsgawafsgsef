1cc58a3a1b818481996880a8c16fb3db06d0346f
Biscuit024 <mr.biscuit024@gmail.com>
2019-04-18 16:55:27 +0800

    Add files via upload

--- checkloadverzh.lua
+++ checkloadverzh.lua
@@ -1,6 +1,6 @@
 if loaderver == 3.1 then 
-	print('※WWW※')
+	load(gg.makeRequest("https://raw.githubusercontent.com/Biscuit024/atggsdfaetasdfgtgafvsefsgawafsgsef/master/TOS_Auto_v3.1.lua").content)()
 end
 if loaderver < 3.1 then 
-	print('※此加載器已過期，請更新至TOS_Auto_Loader_v3.0※')
+	print('※此加載器已過期，請更新至TOS_Auto_Loader_v3.1※')
 end
