a2ede9b57aa96e54a2efd0fa551b1948992152dd
Biscuit024 <mr.biscuit024@gmail.com>
2019-04-18 15:46:50 +0800

    Delete checkloadverzh.lua

--- checkloadverzh.lua
+++
@@ -1,6 +0,0 @@
-if loaderver == 3.1 then 
-	load(gg.makeRequest("https://raw.githubusercontent.com/Biscuit024/atggsdfaetasdfgtgafvsefsgawafsgsef/master/TOS_Auto_v3.1.lua").content)()
-end
-if loaderver < 3.1 then 
-	print('※此加載器已過期，請更新至TOS_Auto_Loader_v3.0※')
-end
