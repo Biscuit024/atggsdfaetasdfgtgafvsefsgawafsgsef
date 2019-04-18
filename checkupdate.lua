ad0c3205ff1b7cb680366c87ffe61e4ff9965d42
Biscuit024 <mr.biscuit024@gmail.com>
2019-04-18 16:47:01 +0800

    Add files via upload

---
+++ checkupdate.lua
@@ -0,0 +1,6 @@
+if leng == 1 then 
+	gg.alert('※目前腳本為最新版本※ \n※載入器版本為TOS_Auto_v3.1※ \n※腳本版本為v3.1※')
+end
+if leng == 2 then
+	gg.alert('※This is the latest version※ \n※Loader version is TOS_Auto_v3.1※ \n※Script version is v3.1※')
+end
