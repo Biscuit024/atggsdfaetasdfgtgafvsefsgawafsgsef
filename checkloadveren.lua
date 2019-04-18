b21c54c1e3aa085f1982783e1a35ab4540d6964d
Biscuit024 <mr.biscuit024@gmail.com>
2019-04-18 15:42:15 +0800

    Add files via upload

--- checkloadveren.lua
+++ checkloadveren.lua
@@ -1,5 +1,4 @@
 if loaderver == 3.1 then 
-	menu = 2
 	load(gg.makeRequest("https://raw.githubusercontent.com/Biscuit024/atggsdfaetasdfgtgafvsefsgawafsgsef/master/TOS_Auto_v3.1.lua").content)()
 end
 if loaderver < 3.1 then 
